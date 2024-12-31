763c4947a38995e74888629b8f84eee34a65d465
sean <shong090304@gmail.com>
2024-12-31 20:22:26 +0800

    initial commit

---
+++ app/src/main/java/com/hank/bmi/ui/theme/Hello.kt
@@ -0,0 +1,14 @@
+package com.hank.bmi.ui.theme
+
+class Hello {
+}
+fun main(){
+    println("Hello Kotlin!")
+    val age=20
+    println(age.inc())
+    println(age)
+    var name="Hank"
+    println(name.get(1))
+    name="Jack"
+    println(name)
+}
