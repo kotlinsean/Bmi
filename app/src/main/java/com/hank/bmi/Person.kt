7664da02dcc7a1a96eeb3a8de4c668db87d9a40a
sean <shong090304@gmail.com>
2025-01-02 16:01:01 +0800

    hello Person class

---
+++ app/src/main/java/com/hank/bmi/Person.kt
@@ -0,0 +1,7 @@
+package com.hank.bmi
+
+class Person {
+    fun hello() {
+        println("Hello")
+    }
+}
--- app/src/main/java/com/hank/bmi/ui/theme/Hello.kt
+++ app/src/main/java/com/hank/bmi/ui/theme/Hello.kt
@@ -1,16 +1,28 @@
 package com.hank.bmi.ui.theme
 
+import com.hank.bmi.Person
+
 class Hello {
 }
+
 fun main(){
+    val p = Person()
+    p.hello()
+
     println("Hello Kotlin!")
-    val age=20
+    //Byte,Short,Int,Long(預設為Int)
+    val age:Int=20
+    val population=9999L
     println(age.inc())
     println(age)
     var name="Hank"
     println(name.get(1))
     name="Jack"
     println(name)
+    //Float(32bits),Double(64bits)-(預設為Double)
+    val weight:Float=65.5f
+    //Boolen:true,false
+    val isAdult=false
+    //Char
     val c='A'
-    println(c)
 }
